e8488cf9ca8e92aafb8436a0a19bc7f0eccc5d8e
Cesaario <gs.cesario1998@gmail.com>
2019-07-08 18:39:35 -0300

    Firebase Instalada

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,9 +1,35 @@
 <template>
-    <h2 class='display-3 text-xs-center'>FIT BATIDO</h2>
+    <v-container>
+        <h2 class='display-3 text-xs-center'>FIT BATIDO</h2>
+        <v-layout align-center justify-center row wrap>
+            <v-flex xs12 md4>
+                <Grafico :chart-data='dados'></Grafico>
+            </v-flex>
+            <v-btn dark color='#1B5E20'>VI UM FIT NÃO BATIDO</v-btn>
+            <v-btn dark color='#B71C1C'>VI UM FIT BATIDO</v-btn>
+            <v-btn dark color='#FBC02D'>VI UM FIT MAS NÃO TENHO CERTEZA</v-btn>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
+    import Grafico from '@/components/Grafico'
     export default {
-
+        components: {
+            Grafico
+        },
+        data(){
+            return{
+                dados: {
+                    labels: ['Não batido', 'Batido', 'Não tenho certeza'],
+                    datasets: [
+                        {
+                            backgroundColor: ['#1B5E20', '#B71C1C', '#FBC02D'],
+                            data: [30, 30, 40]
+                        }
+                    ]
+                }
+            }
+        }
     }
 </script>
---
+++ src/components/Grafico.vue
@@ -0,0 +1,14 @@
+<script>
+    import { Doughnut, mixins } from 'vue-chartjs'
+    const { reactiveProp } = mixins
+
+    export default {
+        name: 'Grafico',
+        extends: Doughnut,
+        mixins: [reactiveProp],
+        props: ['options'],
+        mounted () {
+            this.renderChart(this.chartData, this.options)
+        }
+    }
+</script>
